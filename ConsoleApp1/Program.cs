cc4c8502bc644da712a912419c4ca2e6cbc2d473
hihi-creator <rafiel18326@gmail.com>
2026-09-12 10:05:57 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+int c;
+Console.Write("Nhập a: ");
+a = Convert.ToInt32(Console.ReadLine());
+while (true)
+{
+    Console.Write("Nhập b: ");
+    try
+    {
+        b = int.Parse(Console.ReadLine());
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Số bạn nhập không hợp lệ. Vui lòng nhập lại.");
+    }
+}
+
+bool k;
+while (true)
+{
+    Console.Write("Nhập c: ");
+    k = int.TryParse(Console.ReadLine(), out c);
+    if (k)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Số bạn nhập không hợp lệ. Vui lòng nhập lại.");
+    }
+}
+
+Console.WriteLine($"{a} + {b} + {c} = {a + b + c}");
+
